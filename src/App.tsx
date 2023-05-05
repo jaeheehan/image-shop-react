5bc4a5d3b566a1ef3407a1b6b2f929c98cfbcf99
jaehee <jaehee23@naver.com>
2023-05-06 08:56:47 +0900

    최초 관리자 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage"
 import SignInPage from "./pages/auth/SignInPage";
+import AdminSetupPage from "./pages/member/AdminSetupPage"
 
 function App(){
   return (
       <>
         <Route component={HomePage} path="/" exact/>
         <Route component={SignInPage} path="/signin" exact/>
+        <Route component={AdminSetupPage} path="/member/setup"/>
       </>
   );
 }
