642cc23f695a769c8cacc0e876712df9608b3932
jaehee <jaehee23@naver.com>
2023-05-16 22:40:57 +0900

    리덕스 모듈 정의 #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import React from "react";
 import { Route } from "react-router-dom";
-
 import HomePage from "./pages/HomePage"
 import SignInPage from "./pages/auth/SignInPage";
 import AdminSetupPage from "./pages/member/AdminSetupPage"
 
+import CodeGroupListPage from "./pages/codegroup/CodeGroupListPage";
+import CodeGroupRegisterPage from "./pages/codegroup/CodeGroupRegisterPage";
+import CodeGroupModifyPage from "./pages/codegroup/CodeGroupModifyPage";
+import CodeGroupReadPage from "./pages/codegroup/CodeGroupReadPage";
 export interface LoginInput {
     userId: string;
     password: string;
@@ -27,9 +31,14 @@ export interface CodeGroup {
 function App(){
   return (
       <>
-        <Route component={HomePage} path="/" exact/>
-        <Route component={SignInPage} path="/signin" exact/>
-        <Route component={AdminSetupPage} path="/member/setup"/>
+          <Route component={HomePage} path="/" exact/>
+          <Route component={SignInPage} path="/signin" exact/>
+          <Route component={AdminSetupPage} path="/member/setup"/>
+
+          <Route component={CodeGroupListPage} path="/codegroup" exact/>
+          <Route component={CodeGroupRegisterPage} path="/codegroup/create"/>
+          <Route component={CodeGroupModifyPage} path="/codegroup/edit/:groupCode"/>
+          <Route component={CodeGroupReadPage} path="/codegroup/read/:groupCode"/>
       </>
   );
 }
